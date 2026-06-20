5326ecf38ab9a386384112e4581aed36cdf274d6
Laura Julia Mathias Amaral <laurajulia408@gmail.com>
2026-06-20 14:25:01 -0300

    atualização no layout do site da AgroLink

--- script.js
+++ script.js
@@ -95,3 +95,110 @@ slider.addEventListener("touchmove",(e)=>{
 
 
 });
+
+
+// HERO
+
+const slides = document.querySelectorAll(".slide");
+
+let current = 0;
+
+
+function changeSlide(){
+
+slides.forEach(slide=>{
+slide.classList.remove("active");
+});
+
+
+slides[current].classList.add("active");
+
+}
+
+
+
+setInterval(()=>{
+
+
+current++;
+
+
+if(current >= slides.length){
+
+current = 0;
+
+}
+
+
+changeSlide();
+
+
+},5000);
+
+
+
+
+
+
+// EQUIPE ARRASTAR
+
+
+const team = document.querySelector(".team-grid");
+
+
+let down = false;
+
+let startX;
+
+let scroll;
+
+
+
+team.addEventListener("mousedown",(e)=>{
+
+down = true;
+
+startX = e.pageX - team.offsetLeft;
+
+scroll = team.scrollLeft;
+
+
+});
+
+
+
+team.addEventListener("mouseup",()=>{
+
+down=false;
+
+});
+
+
+
+team.addEventListener("mouseleave",()=>{
+
+down=false;
+
+});
+
+
+
+team.addEventListener("mousemove",(e)=>{
+
+
+if(!down) return;
+
+
+e.preventDefault();
+
+
+const x = e.pageX - team.offsetLeft;
+
+
+const move = (x-startX)*2;
+
+
+team.scrollLeft = scroll - move;
+
+
+});
